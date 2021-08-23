e3ff6a17bf7d8b796ec2a9239489564c1dece1cb
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-08-23 17:37:04 +0800

    update contact weights

--- R/summarise_contacts_weights.R
+++ R/summarise_contacts_weights.R
@@ -4,16 +4,20 @@
 #'
 #' @title
 #' @param data
+#' @param groups
 #' @return
 #' @author Nicholas Tierney
 #' @export
-summarise_contacts_weights <- function(data) {
+summarise_contacts_weights <- function(data, ...) {
 
   data %>% 
+    group_by(date_week,
+             ...) %>% 
     summarise(
       n_resp_weight = sum(n()*weight),
       phys_dist_always_weight = weighted.mean(x = prop_phys_dist_always,
                                               w = weight),
+      phys_not_distancing_weight = (1 -  phys_dist_always_weight),
       across(
         .cols = contains("contact"),
         .fns = ~weighted.mean(x = .x, w = weight),
