f5ebbb60abcf5e6f833e46ca1f9929da0296cd0e
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-08-25 22:10:08 +0800

    add tidyr's complete and expand to conflicts.R

--- conflicts.R
+++ conflicts.R
@@ -4,4 +4,6 @@ conflict_prefer("select", "dplyr")
 conflict_prefer("pluck", "purrr")
 conflict_prefer("map", "purrr")
 conflict_prefer("pull", "dplyr")
+conflict_prefer("complete", "tidyr")
+conflict_prefer("expand", "tidyr")
 weighted_mean <- weighted.mean
