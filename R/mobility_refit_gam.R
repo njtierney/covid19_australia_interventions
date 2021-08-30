b7f0a2322d0498aad2e02fd1270aa0a887fe302e
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-08-30 15:38:18 +0800

    tinkering with refitting the gam model

--- R/mobility_refit_gam.R
+++ R/mobility_refit_gam.R
@@ -8,12 +8,48 @@
 #' @author Nicholas Tierney
 #' @export
 mobility_refit_gam <- function(mobility_gam_which_error) {
-
-  test_data <- mobility_gam_which_error %>%
-    mutate(data_nrow = map_int(data, nrow)) %>% 
-    arrange(-data_nrow) %>% 
-    slice(3) %>%
-    unnest(cols = data)
+  
+  re_fit_gam_mobility <- function(df) {
+    
+    gam(trend ~
+          
+          # smooth variations in mobility
+          # don't set k
+          s(date_num) +
+          
+          # step changes around intervention impositions
+          intervention_stage +
+          
+          # random effect on holidays (different for each holiday, but shrunk
+          # to an average holiday effect which used to predict into future)
+          is_a_holiday +
+          s(holiday, bs = "re") +
+          
+          # constant effect for school holidays
+          is_a_school_holiday +
+          
+          # day of the week effect
+          dow,
+        
+        select = TRUE,
+        gamma = 2,
+        data = df)
+    
+  }
+  
+  safe_re_fit_gam_mobility <- safely(re_fit_gam_mobility)
+  
+  gam_model_fit <- mobility_gam_which_error %>%
+    select(-error_message) %>% 
+    mutate(nrow = map_int(data, nrow)) %>% 
+    arrange(-nrow) %>% 
+    mutate(model_fit = future_map(data, safe_re_fit_gam_mobility)) %>% 
+    ungroup() %>%
+    extract_model_result_error(model_fit) 
+  
+  gam_model_fit %>% filter(did_fit)
+  
+  gam_model_fit %>% filter(!did_fit)
   
   # Bega Valley - Google: time at residential
   # only has intervention data for interventions 2 and 4
